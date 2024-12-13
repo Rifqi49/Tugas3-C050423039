d6841e0336f648ec7c11c9313f711b119ec2e752
Rifqi49 <rifqialfrzaa49@gmail.com>
2024-12-13 17:17:36 +0800

    urutan dsc

--- menu.cpp
+++ menu.cpp
@@ -35,14 +35,25 @@ void tampilkanData(int data[], int n){
 }
 
 void sortingAscending(int data[], int n){
-  for (int i = 0; i < n; i++){
-    for (int j = 0; j < n; j++){
+  for (int i = 0; i < n - 1; i++){
+    for (int j = 0; j < n - i - 1; j++){
       if (data[j] > data [j + 1]){
         tukar(&data[j], &data[j + 1]); 
       }
     }
   }
   cout << "Data berhasil diurutkan secara ascending!";
+}
+
+void sortingDescending(int data[], int n){
+  for (int i = 0; i < n - 1; i++){
+    for (int j = 0; j < n - i - 1; j++){
+      if (data[j] < data [j + 1]){
+        tukar(&data[j], &data[j + 1]); 
+      }
+    }
+  }
+  cout << "Data berhasil diurutkan secara descending!";
 }
 
 int main() {
