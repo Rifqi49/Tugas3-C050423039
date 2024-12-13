873b90f7970b9904d2791f3f3f7d06c5b7269f6e
Rifqi49 <rifqialfrzaa49@gmail.com>
2024-12-13 15:13:41 +0800

    Second Commit

--- menu.cpp
+++ menu.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 
 
+int n;
+
+void tukar(int *a, int *b){
+  int ubah = *a;
+  *a = *b;
+  *b = ubah;
+}
+
 void dMenu(){
 system("cls");
 cout<<"Aplikasi Sorting Bubble"<<"\n";       
-cout<<"1. Masukan Data"<<"\n";            
-cout<<"2. Tampilkan Data"<<"\n";            
-cout<<"3. Sorting asc"<<"\n";           
-cout<<"4. Sorting dsc"<<"\n";            
+cout<<"1. masukan data"<<"\n";            
+cout<<"2. tampilkan data"<<"\n";            
+cout<<"3. sorting asc"<<"\n";           
+cout<<"4. sorting dsc"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -17,12 +25,13 @@ cout<<"Masukan angka :";
 
 void mPertama(string pesan){
 system("cls");
-cout<<"hallo saya menu "<<pesan;
+cout<<"Selamat Datang di Welcome "<<pesan;
 getch();
 }
 
 
 int main() {
+int data[100];
 char pl;
 do
 {
@@ -32,19 +41,45 @@ do
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    //mPertama("pertama");
+    system("cls");
+    cout << "Masukkan jumlah data: ";
+      cin >> n;
+        for (int i=0; i<n; i++){
+          cout << "Masukkan data ke-" << i+1 << ": ";
+          cin >> data[i];
+        }
     break;
    case '2':
-    mPertama("ke- dua");
+    //mPertama("ke- dua");
     /* code */ 
+    system("cls");
+    cout << "Data yang dimasukkan: \n";
+      for(int i=0; i<n; i++){
+        cout << "Data ke-" << i+1 << ": " << data[i] << endl;
+      }
     break;  
    case '3':
-    mPertama("ke- tiga");
+    //mPertama("ke- tiga");
     /* code */
+    void bubble_sort(int data[]){
+      for (int i=1; i<n; i++){
+        for (int j=n-1; j>=i; j--){
+          if(data[j]<data[j-1]) tukar (&data[j], &data[j-1]); //ascending
+        }
+      }
+    }
     break;  
    case '4':
-    mPertama("ke- empat");
+    //mPertama("ke- empat");
     /* code */
+    void bubblesort2(int data[]){
+      for(i=1; i<6; i++){
+        for(int j=0; j<6-i; j++){
+          if(data[j]>data[j+1]) tukar (&data[j], &data[j+1]); //descending
+        }
+      }
+    }
     break;  
   case '5':
     /* code */
